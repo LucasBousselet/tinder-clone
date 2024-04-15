853033744514f06e58c32d8ef1f40fb09d47c858
Lucas Bousselet <bousseletlucas@gmail.com>
2024-04-14 17:44:41 -0700

    RN Google signin

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,15 +1,49 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import * as AuthSession from 'expo-auth-session';
+import {
+    GoogleSignin,
+    GoogleSiginButton,
+    statusCodes
+} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+    scopes: ['profiles', 'email'],
+})
 
 // Creates context with initial state
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-    const signInWithGoogle = async () => {
-    }
+    const [user, setUser] = useState(null);
+
+    _signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            setUser(userInfo);
+        } catch (error) {
+            if (isErrorWithCode(error)) {
+                switch (error.code) {
+                    case statusCodes.SIGN_IN_CANCELLED:
+                        // user cancelled the login flow
+                        break;
+                    case statusCodes.IN_PROGRESS:
+                        // operation (eg. sign in) already in progress
+                        break;
+                    case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+                        // play services not available or outdated
+                        break;
+                    default:
+                    // some other error happened
+                }
+            } else {
+                // an error that's not related to google sign in occurred
+            }
+        }
+    };
 
     return (
-        <AuthContext.Provider value={{ user: null }}>
+        <AuthContext.Provider value={{ user: user, signIn: _signIn }}>
             {children}
         </AuthContext.Provider>
     )
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import useAuth from '../hooks/useAuth'
+import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
 
 export default function LoginScreen() {
+    const { signIn } = useAuth();
+
     return (
         <View>
             <Text>Login</Text>
+            <GoogleSigninButton
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={this.signIn}
+                disabled={this.state.isSigninInProgress}
+            />;
         </View>
     )
 }
